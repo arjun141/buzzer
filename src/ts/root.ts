94ac4db690a95c1811abdc94fda214aaceb456aa
arjun <kovurunagarjuna@gmail.com>
2021-06-20 15:23:25 +0530

    initial commit

---
+++ src/ts/root.ts
@@ -0,0 +1,18 @@
+import * as ko from "knockout";
+import "ojs/ojknockout";
+import Context = require("ojs/ojcontext");
+
+export = class Root {
+  static init(): void {
+    function _init(): void {}
+    // if running in a hybrid (e.g. Cordova) environment, we need to wait for the deviceready
+    // event before executing any code that might interact with Cordova APIs or plugins.
+    if (document.body.classList.contains('oj-hybrid')) {
+      document.addEventListener('deviceready', _init);
+    } else {
+      _init();
+    }
+    // release the application bootstrap busy state
+    Context.getPageContext().getBusyContext().applicationBootstrapComplete();
+  }
+}
